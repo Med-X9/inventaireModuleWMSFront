1662c6f2ef7ce05c8b05052ef6bea49314a4de98
Mohammed BELAZZAB <93041747+Med-X9@users.noreply.github.com>
2025-11-14 23:25:20 +0100

    update

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import axiosInstance from '@/utils/axiosConfig';
 // API base configuration
 const API = {
     baseURL: import.meta.env.VITE_API_BASE_URL,
-    get axiosInstance() {
+    get axiosInstance() { 
         return axiosInstance;
     },
     endpoints: {
